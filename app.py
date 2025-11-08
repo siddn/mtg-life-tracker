040ca2b4bbf7d6461d7db00e78b1d61254b36d75
Siddharth Nathella <snathella@gatech.edu>
2025-11-08 01:07:02 -0500

    Added requirements

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def on_reset(data):
     last_seen[lobby] = time.time()
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
